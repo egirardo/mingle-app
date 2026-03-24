eca1d0beb02dbda51381148e0bb3eac01323a591
Otloir <oli.toreland@gmail.com>
2026-03-24 15:16:09 +0100

    moved the checkboxes into form folder and placed form files into TextImput folder

--- client/src/components/Forms/Checkboxes/Checkbox.jsx
+++ client/src/components/Forms/Checkboxes/Checkbox.jsx
@@ -3,7 +3,7 @@ import { useId } from "react";
 
 export default function Checkbox({ checkboxLabel, id, name }) {
   const inputId = id ?? useId();
-  const inputName = name ?? id;
+  const inputName = name ?? inputId;
   return (
     <div className={styles.checkbox}>
       <input type="checkbox" id={inputId} name={inputName} />
