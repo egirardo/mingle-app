01f4b31c85191215478124c8f7fa02c16837bb08
Elsa Girardo <egirardo@hotmail.com>
2026-03-24 09:17:49 +0100

    Initialized npm in backend file, added server.js file and .env file with db uri

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log(err));
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
