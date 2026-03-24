4def17d96b1329d6aad383d6ea01d85d5d1e4bb0
Elsa Girardo <egirardo@hotmail.com>
2026-03-24 10:23:38 +0100

    Added .env.example file

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log(err));
-
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 // Routes
-app.use('/api/users', require('./routes/userRoutes'));
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.use('/api/users', userRoutes);
