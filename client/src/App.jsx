aaa935ece58461683dfca9d016c00e1d6c0261eb
Elsa Girardo <egirardo@hotmail.com>
2026-03-31 13:38:28 +0200

    Proj Structure: React Router Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,100 +1,22 @@
 import "./App.css";
-import Button from "./components/Buttons/Button.jsx";
-import IconOnlyButton from "./components/Buttons/IconOnlyButton.jsx";
-import TextInput from "./components/Forms/InputFields/TextInput.jsx";
-import Checkbox from "./components/Forms/Checkboxes/Checkbox.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar.jsx";
-import heroImage from "./assets/hero.png";
-import heroShort from "./assets/hero-short.png";
-import TagContainer from "./components/Tags/TagContainer.jsx";
-import TabSlider from "./components/Tabs/TabSlider.jsx";
-import Dropdown from "./components/Dropdown/Dropdown.jsx";
-import SignUpInfo from "./components/LandingPage/SignUpInfo.jsx";
+import Home from "./views/Home.jsx";
+import Login from "./views/Login.jsx";
+import StudentSignUp from "./views/StudentSignUp.jsx";
+import BusinessSignUp from "./views/BusinessSignUp.jsx";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="hero-container">
-        <img src={heroImage} alt="" aria-hidden="true" className="hero-image" />
-      </div>
-      <SignUpInfo />
-      {/* <form> */}
-        {/* --- Buttons --- */}
-
-        {/* <Button buttonName="Log in" iconSrc="arrowRight" />
-        <Button
-          buttonName="Submit"
-          iconSrc="arrowRight"
-          buttonColor="transparent"
-          variant="blackBorder"
-          type="submit"
-        />
-        <Button
-          buttonName="arrow 45"
-          iconSrc="arrow45"
-          buttonColor="transparent"
-          variant="textUnderline"
-        />
-        <Button
-          buttonName="Save"
-          buttonColor="transparent"
-          iconSrc="checkmark"
-          variant="blackBorder"
-        /> */}
-
-        {/* --- Icon Only Buttons --- */}
-
-        {/* <IconOnlyButton
-          iconSrc="search"
-          ariaLabel="Search"
-          buttonColor="transparent"
-        />
-        <IconOnlyButton
-          iconSrc="search"
-          ariaLabel="Search"
-          buttonColor="gray"
-          variant="blackBorder"
-        />
-        <IconOnlyButton
-          iconSrc="arrowBack"
-          ariaLabel="Go back"
-          buttonColor="transparent"
-        />
-        <IconOnlyButton
-          iconSrc="arrow45"
-          ariaLabel="Open link"
-          buttonColor="transparent"
-        /> */}
-
-        {/* --- Inputs --- */}
-
-        {/* <TextInput
-          formLabel="Email"
-          placeholder="skriv.."
-          type="text"
-          id="email"
-        />
-        <TextInput formLabel="Password" placeholder="skriv.." type="password" subText="Lösenordet måste vara minst 8 tecken långt" required={true} optional={true} />
-      </form>
-      <TabSlider
-        tabs={["Företag", "Studenter"]}
-        defaultIndex={0}
-        onChange={(index, label) => console.log(index, label)} // DEV ONLY CONSOLE LOG IS TEMPORARY, REMOVE ONCE FUNCTIONALITY IS IMPLEMENTED
-      />
-      <TagContainer
-        tags={["JavaScript", "React", "CSS", "Node.js", "Express", "MongoDB"]}
-      /> 
-      
-      <Dropdown
-        options={[
-          { value: "utvecklare", label: "Utvecklare" },
-          { value: "designers", label: "Designers" },
-        ]}
-        defaultValue={{ value: "utvecklare", label: "Utvecklare" }}
-        onSelect={(value) => setActiveFilter(value)}
-      /> */}
-    </>
+    <BrowserRouter>
+      <NavBar />  {/* NavBar stays outside Routes so it renders on every page */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup/student" element={<StudentSignUp />} />
+        <Route path="/signup/business" element={<BusinessSignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/views/BusinessSignUp.jsx
@@ -0,0 +1,7 @@
+function BusinessSignUp() {
+  return <div>Business Sign Up</div>;
+}
+
+export default BusinessSignUp;
+
+// temporary content until we have the actual sign up form ready.
