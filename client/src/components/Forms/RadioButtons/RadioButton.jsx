2c8e6d542d8717334669b9e535302e40e0cf9a34
Elsa Girardo <egirardo@hotmail.com>
2026-04-03 12:06:22 +0200

    Fix: Radio Button Component

--- client/src/components/Forms/RadioButtons/RadioButton.jsx
+++ client/src/components/Forms/RadioButtons/RadioButton.jsx
@@ -1,13 +1,14 @@
 import styles from "../Checkboxes/Checkbox.module.css";
 import { useId } from "react";
 
-export default function RadioButtons({ radioLabel, id, name }) {
+export default function RadioButton({ radioLabel, id, name, required = false, value }) {
   const generatedId = useId();
   const inputId = id ?? generatedId;
   const inputName = name ?? inputId;
+  const inputValue = value ?? inputId;
   return (
     <div className={styles.checkbox}>
-      <input type="radio" id={inputId} name={inputName} />
+      <input type="radio" id={inputId} name={inputName} value={inputValue} required={required} />
       <label htmlFor={inputId}>{radioLabel}</label>
     </div>
   );
