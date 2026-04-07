7965bdf78c9eb778c4a312f258f4d57a2e429969
Otloir <oli.toreland@gmail.com>
2026-04-07 11:08:20 +0200

    Added DateTimer, added a span and text that displays either a little welcome text or introduction to the game

--- client/src/views/mingleGame/Introduction.jsx
+++ client/src/views/mingleGame/Introduction.jsx
@@ -1,25 +1,39 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate, useOutletContext } from "react-router-dom";
+import { useState } from "react";
 import styles from "./MingleGame.module.css";
-import Timer from "../../components/Timer/Timer.jsx";
+import DateTimer from "../../components/Timer/DateTimer.jsx";
 
 export default function Introduction() {
-  const navigate = useNavigate();
+  const [expired, setExpired] = useState(false);
+  
 
   const handleExpire = () => {
-    // When the timer expires, start the game on the task page
+    // When the timer expires, show follow-up text (and eventually start the game)
+    setExpired(true);
+    // to do: if server starts game, make it navigate to task
     // navigate("/task");
   };
 
   return (
     <div className={styles.mingleIntroduction}>
       {/* add app logo here */}
-      <h1 className={ styles.introductionHeader}>Welcome to Ignite Speed Mingle</h1>
-      {/* date timer instead, temporary countdown timer for now*/}
-      <Timer minutes={0.1} onExpire={handleExpire} />
-      <p className="introductionText">
-        For the next 10 minutes, you’ll have short and fast interactions. We’ll
-        guide you step by step.
-      </p>
+      <span className={styles.introductionHeader}>Welcome to</span>
+      <h1 className={styles.introductionHeader}>
+        Welcome to Ignite Speed Mingle
+      </h1>
+      <DateTimer targetDate="04/06/2026 22:11" onExpire={handleExpire} />
+      {/* <DateTimer targetDate="04/22/2026 15:00" /> */}
+      {!expired ? (
+        <p className="introductionText">
+          For the next 10 minutes, you’ll have short and fast interactions.
+          We’ll guide you step by step.
+        </p>
+      ) : (
+        <p className="introductionText">
+          Get ready to meet new people. Follow the instructions on your phone
+          when the countdown drops.
+        </p>
+      )}
     </div>
   );
 }
