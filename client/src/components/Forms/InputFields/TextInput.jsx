c79cd6a59f9e600cc9d760c81f550bc9434653d9
Elsa Girardo <egirardo@hotmail.com>
2026-04-08 16:18:05 +0200

    Fixes: Input Field/Panel Styling

--- client/src/components/Forms/InputFields/TextInput.jsx
+++ client/src/components/Forms/InputFields/TextInput.jsx
@@ -24,7 +24,7 @@ export default function TextInput({
   const label = formLabel ? (
     <label htmlFor={inputId} className={styles.inputLabel}>
       {formLabel}
-      {required && <span aria-hidden="true" style={{ color: "grey", fontStyle: "italic" }}> *</span>}
+      {required && <span aria-hidden="true" style={{ color: "#D5D5D5", fontStyle: "italic" }}> *</span>}
       {optional && <span aria-hidden="true" className={styles.optional}> (optional)</span>}
     </label>
   ) : null;
