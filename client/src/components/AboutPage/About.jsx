657eec4643ddf2f7ed251aadb6f4e0ba1aab1b78
Otloir <oli.toreland@gmail.com>
2026-04-13 11:39:28 +0200

    updated the layout to be more semantic, changed some class names to make it more readable and restructured the css file

--- client/src/components/AboutPage/About.jsx
+++ client/src/components/AboutPage/About.jsx
@@ -3,43 +3,43 @@ import styles from "./About.module.css";
 
 export default function About() {
   return (
-    <section className={`${styles.aboutPanel}`}>
-      <div className={styles.aboutHeader}>
+    <section className={styles.aboutPanel}>
+      <header className={styles.aboutHeader}>
         <h1>LIA FUSION</h1>
-        <h3>Meaningful connections start here</h3>
-      </div>
-      <div className={styles.aboutTextContainer}>
-        <div className={styles.aboutTextContainer2}>
-          <div className={styles.aboutText}>
-            <h4>ABOUT</h4>
-            <p>
-              LIA FUSION helps students and companies connect before, during and
-              after the LIA event. Explore profiles, start conversations, and
-              find the right match.
-            </p>
-            <div className={styles.aboutSlogan}>
-              <p>Less randomness.</p>
-              <p>More meaningful connections.</p>
-            </div>
+        <h2>Meaningful connections start here</h2>
+      </header>
+
+      <div className={styles.aboutContent}>
+        <article className={styles.aboutSection}>
+          <h3>ABOUT</h3>
+          <p>
+            LIA FUSION helps students and companies connect before, during and
+            after the LIA event. Explore profiles, start conversations, and find
+            the right match.
+          </p>
+          <div className={styles.slogan}>
+            <p>Less randomness.</p>
+            <p>More meaningful connections.</p>
           </div>
-          <div className={styles.aboutText}>
-            <h4>CONTACT AND SUPPORT</h4>
-            <p>We’re here to support you before, during and after the event.</p>
-            <p>
-              If you need to change information in your profile, please send us
-              an email and we will update your profile for you.
-            </p>
-            <div className={styles.emailButton}>
-              <a href="mailto:ac75456@skola.goteborg.se">
-                <Button
-                  buttonName="Contact us"
-                  variant="primaryGray"
-                  iconSrc="arrow45"
-                />
-              </a>
-            </div>
+        </article>
+
+        <article className={styles.aboutSection}>
+          <h3>CONTACT AND SUPPORT</h3>
+          <p>We're here to support you before, during and after the event.</p>
+          <p>
+            If you need to change information in your profile, please send us an
+            email and we will update your profile for you.
+          </p>
+          <div className={styles.contactButton}>
+            <a href="mailto:ac75456@skola.goteborg.se">
+              <Button
+                buttonName="Contact us"
+                variant="primaryGray"
+                iconSrc="arrow45"
+              />
+            </a>
           </div>
-        </div>
+        </article>
       </div>
     </section>
   );
